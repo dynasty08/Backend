c8fab3de62fd859637875ea3a717acb163881de2
dynasty08 <dynastygateway08@fhbpo.com>
2025-08-07 16:58:15 +0800

    added hello.js

--- lambda/hello.js
+++ lambda/hello.js
@@ -1,7 +1,7 @@
-// lambda/hello.js
-//exports.handler = async (event) => {
-  //return {
-    //statusCode: 200,
-    //body: JSON.stringify({ message: "Hello from Lambda!" }),
-  //};
-//};
+lambda/hello.js
+exports.handler = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hello from Lambda!" }),
+  };
+};
